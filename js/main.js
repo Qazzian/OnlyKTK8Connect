0796ec5d93d9eb063d7237732c69dfc420c321a3
Ian Wallis <ian.wallis@grapplemobile.com>
2011-11-02 23:29:40 +0000

    Lots more function, fixes and graphics created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 OnlyConnect = {
 
 	puzzle: null,
+	playing: false,
 	selectedBlocks: [],
 	groups: [],
+	TIME_LIMIT: 3*60*1000,
+	startTime: 0,
+	LIFE_LIMIT: 3,
+	lives: 3,
+
+	// TODO time limit
 	
 	init: function(){
 		OnlyConnect.puzzle = Puzzles.getPuzzle();
@@ -12,6 +19,7 @@ OnlyConnect = {
 			$(this).bind('click',OnlyConnect.selectWall);
 		});
 		$('.wallOption').live('click', OnlyConnect.selectWallBlock);
+		$('.revealAnswers').addClass('hidden');
 	},
 
 	showWallSelector: function(){
@@ -26,6 +34,11 @@ OnlyConnect = {
 		var wallData = OnlyConnect.puzzle.getUnsortedWall(selectedWall);
 		console.log(wallData);
 		OnlyConnect.buildWall(wallData);
+		// TODO start the timer
+		OnlyConnect.lives = OnlyConnect.LIFE_LIMIT;
+		OnlyConnect.playing = true;
+		OnlyConnect.startTimer();
+
 	},
 
 	buildWall: function(wallData){
@@ -41,12 +54,20 @@ OnlyConnect = {
 	selectWallBlock: function(event){
 		var $this = $(this),
 			oc = OnlyConnect;
-		if ($this.hasClass('selected') || $this.hasClass('set')) {
+		if ($this.hasClass('timeout') || $this.hasClass('set')) {
 			return;
 		}
-		$this.addClass('selected').addClass('group'+oc.groups.length);
-		OnlyConnect.selectedBlocks.push(OnlyConnect.getBlockValue($this));
-		OnlyConnect.checkForGroup(OnlyConnect.selectedBlocks);
+		var value = oc.getBlockValue($this);
+		if ($this.hasClass('selected')) {
+			var index = oc.selectedBlocks.indexOf(value);
+			oc.selectedBlocks.splice(index, 1);
+			$this.removeClass('selected').removeClass('group'+oc.groups.length);
+		}
+		else {
+			$this.addClass('selected').addClass('group'+oc.groups.length);
+			oc.selectedBlocks.push(value);
+			oc.checkForGroup(oc.selectedBlocks);
+		}
 	},
 
 	getBlockValue: function($elmt){
@@ -62,11 +83,24 @@ OnlyConnect = {
 		if (oc.puzzle.isGroup(group)) {
 			oc.storeSavedBlocks(group);
 			oc.selectedBlocks = [];
-			// TODO if group 2 then activate 3 lives.
 			// TODO if group 3 then do last group automatically and end the game
+			if (oc.groups.length == 3) {
+				oc.endGame(true);
+			}
 		}
 		else {
 			oc.clearSelectedBlocks();
+			oc.looseLife();
+		}
+	},
+
+	looseLife: function(){
+		var oc = OnlyConnect;
+		if (oc.groups.length == 2){
+			oc.lives--;
+		}
+		if (oc.lives <= 0) {
+			oc.endGame(false);
 		}
 	},
 
@@ -94,36 +128,55 @@ OnlyConnect = {
 				.addClass('set')
 				.remove();
 		// TODO sort the groupElmts
+		oc.sortBlocks(groupElmts);
 		var targetIndex = (groupId*4);
 		var target = wallList.children()[targetIndex];
 		groupElmts.insertBefore(target);
 	},
 
 	sortBlocks: function(group){
-		// TODO
+		var oc = OnlyConnect;
+		var compare = function(A, B) {
+			var a = (oc.getBlockValue($(A))+'').toLowerCase(),
+				b = (oc.getBlockValue($(B))+'').toLowerCase();
+			return a >= b ? 1 : -1;
+		}
+		return group.sort(compare);
 	},
 
-	moveBlock: function(block, row, col){
-		var $this = $(block),
-			blockId = $this.attr('id'),
-			$wall = $('#wall ul'),
-			$allOptions = wall.find('li'),
-			newPos = row*4 + col,
-			newTop = blockHeight * row,
-			newLeft = blockWidth * col,
-			currPos, currTop, currLeft;
+	startTimer: function(){
+		OnlyConnect.startTime = Date.now();
+		OnlyConnect.checkTime();
+	},
 
-		// find the current pos
-		$wall.each(function(index){
-			if (this.id == blockId) {
-				currPos = index;
-				return false;
-			}
-		});
+	checkTime: function(){
+		var oc = OnlyConnect,
+			timeUsed = Date.now() - oc.startTime;
 
-		// Now to do the move ???
-			
+		if (timeUsed >= oc.TIME_LIMIT) {
+			oc.endGame(false);
+		}
+		else {
+			setTimeout(oc.checkTime, 500);
+		}
+		var time_pc = timeUsed/oc.TIME_LIMIT * 100;
+		$('.TimeUsed').css('width', time_pc+'%');
+	},
 
+	endGame: function(isWin) {
+		if (isWin) {
+			$('#wall ul li').not('.set').each(OnlyConnect.selectWallBlock);
+			alert("you completed the wall.");
+		}
+		else {
+			var $wall = $('#wall ul'),
+				$missedOptions = $wall.find('li').not('.set');
+			$missedOptions.removeClass('selected').addClass('timeout');
+			$('.revealAnswers').removeClass('hidden');
+		}
+	},
+
+	revealAnswers: function(){
 
 	}
 	
